e9e182874c52f88dcb5586d1156a87569001ab91
epochong <Valentineone@example.com>
2019-07-01 11:17:29 +0800

    first

---
+++ src/学分绩点.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * @author epochong
+ * @date 2019/7/1 9:17
+ * @email epochong@163.com
+ * @blog epochong.github.io
+ * @describe
+ */
+public class 学分绩点 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        while (input.hasNextInt()) {
+            int n = input.nextInt();
+            ArrayList<Integer> studyScore = new ArrayList <>();
+            ArrayList<Integer> score = new ArrayList <>();
+            for (int i = 0; i < ; i++) {
+                
+            }
+            
+        }
+    }
+}
